11ea17720d894ffc1e6752122d8f5ef1e927a1ed
bavarianchocolate <35240485+bavarianchocolate@users.noreply.github.com>
2022-11-30 22:22:17 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let varNum = 123456;
+let varString = 'IT School Hillel';
+let varBoolean = false;
+
+console.log('Type of my variables: varNum is ' + typeof varNum, '|', 'varString is ' + typeof varString, '|', 'varBoolean is ' + typeof varBoolean);
+
+console.log('Value of varNum is ' + varNum, 'and Type is ' + typeof varNum);
+console.log('Value of varString is ' + varString, 'and Type is ' + typeof varString);
+console.log('Value of varBoolean is ' + varBoolean, 'and Type is ' + typeof varBoolean);
+
+alert ('This is my first homework on course FrontEnd Pro'); 
